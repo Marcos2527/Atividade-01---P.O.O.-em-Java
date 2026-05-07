19435fcece418f28acd4f65476acbafcb21a022b
Marcos2527 <marcos.saturnino1@aluno.ce.gov.br>
2026-05-07 07:38:18 -0300

    Add files via upload

--- Q10At1.java
+++ Q10At1.java
@@ -1,23 +1,20 @@
 import java.util.Scanner;
 
 public class Q10At1 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int tempo, segundos, minutos;
-        double horas;
+        System.out.println("Informe o tempo (em segundos):");
+        int tempo = scanner.nextInt();
 
-        System.out.println("Informe o tempo (em segundos): ");
-        tempo = scanner.nextInt();
+        int horas = tempo / 3600;
+        int resto = tempo % 3600;
+        int minutos = resto / 60;
+        int segundos = resto % 60;
 
-        segundos = tempo;
-        minutos = segundos / 60;
-        horas = minutos / 60;
-
-        System.out.println("Tempo em segundos: " + segundos + " seg.");
-        System.out.println("Tempo em minutos: " + minutos + " min.");
-        System.out.println("Tempo em horas: " + String.format("%.2f", horas) + " h.");
-        System.out.println("(Valores aproximados)");
+        // Essa lógica teve ajuda da IA
+        System.out.printf("Tempo: %02d:%02d:%02d\n", horas, minutos, segundos);
 
         scanner.close();
     }
